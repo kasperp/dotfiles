494a88805658baaca0a93a624e8fc5d6d9adde92
Kasper Bøgebjerg Pedersen <kasperbp@gmail.com>
2013-01-20 20:57:16 +0100

    adds comments!

--- sync.sh
+++ sync.sh
@@ -2,19 +2,22 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
-	cd subl
-	rsync -av . ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
-	cd ..
+  # sync dotfiles to ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
+
+  # sync sublime settings
+  cd subl
+  rsync -av . ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+  cd ..
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 unset doIt
 source ~/.bash_profile
