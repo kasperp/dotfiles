20d09d7da289a9b65cf374342382e45ebf538dd2
Kasper Bøgebjerg Pedersen <kasper@configit.com>
2013-10-14 21:40:28 +0200

    Removes nave

--- install-deps.sh
+++ install-deps.sh
@@ -9,11 +9,6 @@ sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh
 # rvm for the rubiess
 curl -L https://get.rvm.io | bash -s stable --ruby
 
-# https://github.com/isaacs/nave
-# needs npm, obviously.
-# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
-
 # homebrew!
 # you need the code CLI tools YOU FOOL.
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
