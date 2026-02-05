26944dbc437b560a4a5902506472b1d82755f851
Kasper Bøgebjerg Pedersen <481303+kasperp@users.noreply.github.com>
2026-02-05 08:40:58 +0100

    Move machine-specific aliases to local.zsh (untracked)

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,10 +9,6 @@ alias lt='ls --tree --level=3'
 # Shell
 alias reload='source ~/.zshrc'
 
-# Remote development
-alias scode='code --new-window --remote ssh-remote+kasper@46.4.77.8'
-alias scursor='cursor --new-window --remote ssh-remote+kasper@46.4.77.8'
-
 # Tools
 alias proc=btop
 alias gtr=git-gtr
